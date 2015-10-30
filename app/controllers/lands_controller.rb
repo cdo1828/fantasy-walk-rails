c3664dbeba7db29a4286618454c7c6c1e0e27eea
Daniel T <danthwa@gmail.com>
2015-10-29 21:34:23 -0700

    setting up root routes and controllers for land descriptions

---
+++ app/controllers/lands_controller.rb
@@ -0,0 +1,5 @@
+class LandsController < ApplicationController
+	def Rivendell
+		#@friend_achievements =
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root 'lands#rivendell'
+
   get 'sessions/create'
 
   get 'sessions/destroy'
